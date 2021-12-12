e2c8c7f977a41fd00e247864dbac9e258eebcf36
MoyusiteruIori <y.meng1@outlook.com>
2021-12-12 23:26:09 +0800

    增加单词

--- rsc/component/plugins/tango/__init__.py
+++ rsc/component/plugins/tango/__init__.py
@@ -13,7 +13,7 @@ async def write_eng(session:CommandSession):
     else:
         await session.send(f'笨蛋，答案是{right_eng}')
         
-@on_command('单选单词', aliases=('!tango'))
+@on_command('汉译选英', )
 async def choose_eng(session:CommandSession):
     answers = rd_choose(4)
     ans_dict = dict(zip([word[0] for word in answers], ['A','B','C','D']))
@@ -45,3 +45,42 @@ async def tango_combo(session:CommandSession):
             await session.send(f'笨蛋，答案是{right_eng}')
         await asyncio.sleep(0.5)
     await session.send('结束~~')
+    
+@on_command('英文选义')
+async def choose_chn(session:CommandSession):
+    answers = rd_choose(4)
+    ans_dict = dict(zip([word[1] for word in answers], ['A','B','C','D']))
+    right_ans = random.choice(answers)
+    answer = (await session.aget(
+        prompt=f'哼~哼~哼~\n「{right_ans[0]}」是什么意思呢？\n'
+        + f'A {answers[0][1]}\n'
+        + f'B {answers[1][1]}\n'
+        + f'C {answers[2][1]}\n'
+        + f'D {answers[3][1]}\n'
+        ))
+    if(answer[0] == ans_dict[right_ans[1]]):
+        await session.send('答对啦')
+    else:
+        await session.send(f'笨蛋，答案是「{right_ans[1]}」')
+        
+
+@on_command('英文选义combo')
+async def choos_chn_combo(session:CommandSession):
+    while True:
+        answers = rd_choose(4)
+        ans_dict = dict(zip([word[1] for word in answers], ['A','B','C','D']))
+        right_ans = random.choice(answers)
+        answer = (await session.aget(
+            prompt=f'哼~哼~哼~\n「{right_ans[0]}」是什么意思呢？\n'
+            + f'A {answers[0][1]}\n'
+            + f'B {answers[1][1]}\n'
+            + f'C {answers[2][1]}\n'
+            + f'D {answers[3][1]}\n'
+            ))
+        if(answer == 'stop'):
+            break
+        if(answer[0] == ans_dict[right_ans[1]]):
+            await session.send('答对啦')
+        else:
+            await session.send(f'笨蛋，答案是「{right_ans[1]}」')
+    await session.send('结束~~~')
